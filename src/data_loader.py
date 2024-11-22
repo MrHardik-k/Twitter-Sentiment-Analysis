d46e988b9fb83473de670870966ea6399adcb9bc
Soumya Dhakad <soumyadhakad123@gmail.com>
2024-11-22 20:45:45 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -16,18 +16,14 @@ for line in content:
     embeddings[word] = embedding
 
 <data load from kaggle>
-# load  everything from files
 import pickle
 with open('word2index.pickle', 'rb') as handle:
     word2index = pickle.load(handle)
 embedding_matrix = np.load('embedding_metrix.npy')
-# with open('tokenizer.pickle', 'rb') as handle:
-#     tokenizer = pickle.load(handle)
 Xtrain = np.load('Xtrain.npy')
 Xtest = np.load('Xtest.npy')
 Ytrain = np.load('Ytrain.npy')
 Ytest = np.load('Ytest.npy')
-# model = load_model('model.h5')
 history = np.load('history.npy', allow_pickle='TRUE').item()
 with open('tokenizer.json', 'r', encoding='utf-8') as f:
     tokenizer_json = json.load(f)
@@ -74,18 +70,12 @@ for word, i in word2index.items():
 lemmatizer=WordNetLemmatizer()
 
 def preprocessing(text):
-    # Ensure the input is a string, otherwise return an empty string
     if not isinstance(text, str):
         return ''
 
-    # Use regex to match words starting with @ or #
-    # remove URLs
+    cleaned_text = re.sub(r'(http|https|www)\S+', '', text) 
+    cleaned_text = re.sub(r'[@#]\w+', '', cleaned_text)
 
-    cleaned_text = re.sub(r'(http|https|www)\S+', '', text)  # Remove URLs
-    cleaned_text = re.sub(r'[@#]\w+', '', cleaned_text) # Remove mentions (like @username) and hashtgs
-    # cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
-
-    # remove every charactor which is not alphbet
     cleaned_text = re.sub(r'[^a-zA-Z\s]', '', text)
     cleaned_text = cleaned_text.replace('\n', ' ')
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
@@ -93,12 +83,6 @@ def preprocessing(text):
     cleaned_text = word_tokenize(cleaned_text)
     filtered_words = [lemmatizer.lemmatize(word, pos='v') for word in cleaned_text]
 
-    # remove word if it's not in embeddings
-    # new_filtered_words = [word for word in filtered_words if word in embeddings]
-    # if len(new_filtered_words) != len(filtered_words):
-    #     return None
-
-    # Join the words back into a single string
     text = ' '.join(filtered_words)
     return text
 
